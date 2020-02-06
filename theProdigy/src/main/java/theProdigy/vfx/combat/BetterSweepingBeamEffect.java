3e11ec3c88cc2e28020b2d003e5c905d29cc718b
erasels <emanuelbedburdick@yahoo.de>
2020-02-06 21:40:59 +0100

    Update BetterSweepingBeamEffect.java

--- theProdigy/src/main/java/theProdigy/vfx/combat/BetterSweepingBeamEffect.java
+++ theProdigy/src/main/java/theProdigy/vfx/combat/BetterSweepingBeamEffect.java
@@ -1,10 +1,15 @@
 package theProdigy.vfx.combat;
 
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.vfx.combat.SweepingBeamEffect;
 import theProdigy.util.UC;
 
 public class BetterSweepingBeamEffect extends SweepingBeamEffect {
+    public BetterSweepingBeamEffect(AbstractCreature c, Color col) {
+        this(c.hb.cX + (c.hb.width/4f), c.hb.cY, col);
+    }
+
     public BetterSweepingBeamEffect(float sX, float sY, Color col) {
         super(sX, sY, UC.p().flipHorizontal);
         this.color = col;
